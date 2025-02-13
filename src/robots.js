356fda20ae5072b61d8edaeb23a52c2dde582830
stephen <isaacsteverado9@gmail.com>
2025-02-13 01:56:19 +0100

    built a dynamic robot card

---
+++ src/robots.js
@@ -0,0 +1,28 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'leanne graham',
+        username: 'Bret',
+        email: 'leanee@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'lukas mark',
+        username: 'Bret',
+        email: 'leanee@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'kate adams',
+        username: 'adms',
+        email: 'leanee@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'micheal wiliams',
+        username: 'micheal',
+        email: 'leanee@gmail.com'
+    
+    }
+
+] 

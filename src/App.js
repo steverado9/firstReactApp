e09349edbb53171fa57a5886c42fd0b48ed71758
stephen <isaacsteverado9@gmail.com>
2025-02-15 01:05:39 +0100

    updated robofriend app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import react, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 
 class App extends Component { //extended App component
@@ -13,16 +14,19 @@ class App extends Component { //extended App component
         }
     }
 
-    onSearchChange(event) {
-        console.log(event.target.value);
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});        
     }
 
     render() {
+        const filteredRobot = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={this.state.robots} />
+                <CardList robots={filteredRobot} />
             </div>
         );
     }
--- src/robots.js
+++ src/robots.js
@@ -9,54 +9,54 @@ export const robots = [
         id: 2,
         name: 'lukas mark',
         username: 'mark',
-        email: 'leanee@gmail.com'
+        email: 'lukas@gmail.com'
     },
     {
         id: 3,
         name: 'kate adams',
         username: 'adms',
-        email: 'leanee@gmail.com'
+        email: 'kate@gmail.com'
     },
     {
         id: 4,
         name: 'micheal wiliams',
         username: 'micheal',
-        email: 'leanee@gmail.com'
+        email: 'micheal@gmail.com'
     },
     {
         id: 5,
         name: 'jason statam',
         username: 'jason',
-        email: 'leanee@gmail.com'
+        email: 'jason@gmail.com'
     },
     {
         id: 6,
         name: 'micheal jackson',
         username: 'admjacksons',
-        email: 'leanee@gmail.com'
+        email: 'jackson@gmail.com'
     },
     {
         id: 7,
         name: 'rose ameldia',
         username: 'rose',
-        email: 'leanee@gmail.com'
+        email: 'rose@gmail.com'
     },
     {
         id: 8,
         name: 'sam smith',
         username: 'smith',
-        email: 'leanee@gmail.com'
+        email: 'smith@gmail.com'
     },
     {
         id: 9,
         name: 'isaac stephen',
         username: 'stephen',
-        email: 'leanee@gmail.com'
+        email: 'isaac@gmail.com'
     },
     {
         id: 10,
         name: 'muffin favour',
         username: 'favour',
-        email: 'leanee@gmail.com'
+        email: 'favour@gmail.com'
     }
 ] 
